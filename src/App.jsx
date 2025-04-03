ebe8493c0ac45e1fc322af8f43660c8fe4ab8f8e
Purvanshu10 <purvanshujindal10@gmail.com>
2025-04-04 01:23:14 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ const [post, setpost] = useState([])
+ const fetchData = async () => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data = await a.json()
+    console.log(data);
+    setpost(data)
+ }
+ useEffect(() => {
+   fetchData()
+ }, [])
+ 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container">
+        {post.map((post)=>{
+          return <div className="cards" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div> 
+        })}
+    </div>
     </>
-  )
-}
+  );
+};
 
 export default App
